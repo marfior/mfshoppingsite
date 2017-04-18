bcecfb1c71ede8e1384e407aa9c19222eb735e67
Martin <Martin>
2017-04-19 00:01:40 +0200

    Added signing in

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import {Router} from '@angular/router'
+
+import { User } from "../Services/Models/user";
+import { UserService } from "../Services/user.service";
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-signin',
@@ -7,17 +12,53 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+	
+	private user: User = new User(0,"","","","");
+	private incorrectLogin: boolean = false;
+	private signingIn: boolean = false;
+	
+	items: FirebaseListObservable<any[]>;
 
-  constructor() { }
+	constructor(private userService: UserService, private router: Router) {
+		
+	}
 
-  ngOnInit() {
-  }
-  
-  onSubmit(form)
-  {
-	  
-	  debugger;
-		console.log(form);
-  }
+	ngOnInit() {
+	}
+
+	onSubmit(form)
+	{
+		this.signingIn = true;
+		this.user.email = form.controls.email.value;
+		this.user.password = form.controls.password.value;
+		
+		//debugger;
+		this.userService.getUserByEmail(this.user)
+				.subscribe(	(userRes) => {
+							//console.log(userRes)
+							if (userRes != null && this.user.password == userRes.password)
+							{
+								//this.router.navigateByUrl('home');
+							}
+							else
+							{
+								//this.router.navigateByUrl('signin');
+								this.incorrectLogin = true;
+							}
+							this.signingIn = false;
+
+				}
+		);
+
+			
+			/*.then( (usr) => {
+			if (usr.id > 0)
+				
+			else
+				
+		});*/
+
+		
+	}
 
 }
