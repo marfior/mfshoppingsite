fdd841a8fb169ee52fa332de5a3026ecc632f762
Martin <Martin>
2017-04-19 00:01:02 +0200

    Added registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
+
+import {FormGroup, FormBuilder, FormControl, Validators} from "@angular/forms";
+
+import { UserService } from "../Services/user.service";
+import { User } from "../Services/Models/user";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+	
+	registrationGroup: FormGroup;
 
-  constructor() { }
+	passwordControl = new FormControl('',Validators.required);
+	confirmpassControl = new FormControl('',Validators.required);
+  
+	private user: User = new User(0,"","","","");
+	private registering: boolean = false;
+	
 
-  ngOnInit() {
-  }
+	constructor(private userService: UserService, private router: Router) { 
+	}
 
+	ngOnInit() {
+	}
+
+	onSubmit(form)
+	{
+		this.registering = true;
+		this.user.email = form.controls.email.value;
+		this.user.name = form.controls.name.value;
+		this.user.surname = form.controls.surname.value;
+		this.user.password = form.controls.password.value;
+		
+		this.userService.addUser(this.user);
+		
+		//this.router.navigateByUrl('signin');
+		//console.log(user);
+		//console.log(this.myGroup);
+		//console.log(this.myGroup.value.name);
+		//debugger;
+
+
+	}
 }
