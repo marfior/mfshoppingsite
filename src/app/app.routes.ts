5531676ac6ccf9a6ee5de9664bb9626e6f276f21
Martin <Martin>
2017-04-24 01:49:45 +0200

    Added product routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,13 @@ import {Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {SigninComponent} from "./signin/signin.component";
 import {RegisterComponent} from "./register/register.component";
-
-
-/**
- * Created by stc2 on 15/02/2017.
- */
+import {ProductlistComponent} from "./productlist/productlist.component";
+import {ProductComponent} from "./product/product.component";
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'signin', component: SigninComponent},
   { path: 'register', component: RegisterComponent},
+  { path: 'productlist/:category', component: ProductlistComponent},
+  { path: 'product/:category/:key', component: ProductComponent},
 ];
