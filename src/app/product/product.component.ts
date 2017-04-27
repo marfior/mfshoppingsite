5e3ce0728264587f897aa0bf12287fbd77ecb820
Martin <Martin>
2017-04-27 02:27:35 +0200

    Design of product page and changes

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product } from "../Services/Models/product";
+import { Shoppingcart } from "../Services/Models/shoppingcart";
 import { ProductService } from "../Services/product.service";
-import { ShoppingcartService } from "../Services/shoppingcart.service";
+import { ProductcartService } from "../Services/productcart.service";
+import { UserService } from "../Services/user.service";
 
 @Component({
   selector: 'app-product',
@@ -12,30 +14,32 @@ import { ShoppingcartService } from "../Services/shoppingcart.service";
 })
 export class ProductComponent implements OnInit {
 
+  private addingToCart: boolean = false;
+
   key: string;
   category: string;
-
   quantity: number;
-
   product: Product;
 
-  constructor(private productService: ProductService,private shoppingcart: ShoppingcartService,private actRoute: ActivatedRoute) { 
+  constructor(private productService: ProductService
+              ,private userService: UserService
+              ,private productcartService: ProductcartService
+              ,private actRoute: ActivatedRoute) { 
    
   }
 
   ngOnInit() {
 
     this.actRoute.params.subscribe(par => {
-        this.category = par["category"];
         this.key = par["key"];
-      
-        this.productService.getProductByKey(this.category,this.key).subscribe(
+        
+        this.productService.getProductByKey(this.key).subscribe(
           
-          (arrResults) => this.product = new Product(arrResults[0].$key,arrResults[0].name
+          (arrResults) => this.product = arrResults[0] 
+          /*new Product(arrResults[0].$key,arrResults[0].name
                                                       ,arrResults[0].price,
-                                                      this.category,arrResults[0].description)
+                                                      this.category,arrResults[0].description)*/
 
-            //this.test = "ddddd";
         );
 
       });
@@ -45,8 +49,10 @@ export class ProductComponent implements OnInit {
 
   public addToCart()
   {
+      this.addingToCart = true;
+      this.productcartService.addProductCart(this.userService.userLogged.$key,this.key,this.quantity);
 
-
+      this.addingToCart = false;
   }
 
 }
