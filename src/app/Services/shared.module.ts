f541088abcc0f10f4b469594364406b0684aad9d
Martin <Martin>
2017-04-27 02:08:44 +0200

    Added Productcart service

--- src/app/Services/shared.module.ts
+++ src/app/Services/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { AngfirebaseService } from "./angfirebase.service";
 
 import { User } from "./Models/user";
@@ -9,12 +8,13 @@ import { UserService } from "./user.service";
 
 import { Product } from "./Models/product";
 import { ProductService } from "./product.service";
+import { ProductcartService } from "./productcart.service";
 
 @NgModule({
   imports: [
 	CommonModule,
   ],
-  providers: [UserService, AngfirebaseService, ProductService],
+  providers: [UserService, AngfirebaseService, ProductService, ProductcartService],
   declarations: []
 })
 export class SharedModule { }
