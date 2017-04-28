fe1710568667404224f72d3d895a2d6b54d49f04
Martin <Martin>
2017-04-28 03:26:29 +0200

    Added shoppingcart phase1

--- src/app/shoppingcart/shoppingcart.component.ts
+++ src/app/shoppingcart/shoppingcart.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from "../Services/Models/product";
+import { ProductService } from "../Services/product.service";
+import { ProductcartService } from "../Services/productcart.service";
+import { UserService } from "../Services/user.service";
 
 @Component({
   selector: 'app-shoppingcart',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingcartComponent implements OnInit {
 
-  constructor() { }
+  private arrProductscart = [];
+  private arrProducts = [];
+
+  constructor(private productService: ProductService
+            , private userService: UserService
+            , private productcartService: ProductcartService) { }
 
   ngOnInit() {
+
+    //if (this.userService.userLogged.$key != undefined) {
+//.map( (productsRes) => this.countProductscart = productsRes.length )
+
+                       debugger;
+  this.productService.getAllProducts()
+                    .subscribe( (productsRes) => {
+                        this.arrProducts = productsRes 
+                        this.productcartService.getProductscartByUser(this.userService.userLogged.$key)
+                            .subscribe( (productscartRes) => {
+                              this.arrProductscart = productscartRes;
+                            })
+                    });
+
+
   }
+
 
 }
