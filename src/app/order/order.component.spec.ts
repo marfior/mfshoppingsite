cbe7b3f7828316647b721268b66be79c417ea75f
Martin <Martin>
2017-04-30 02:16:24 +0200

    Testing order component

--- src/app/order/order.component.spec.ts
+++ src/app/order/order.component.spec.ts
@@ -1,9 +1,17 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed,inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import {Observable} from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+
 import { OrderComponent } from './order.component';
+
+
+class FakeActivatedRoute {
+  params: Observable<any> = Observable.from([{key: "fakeorderkey"}]);
+}
 
 describe('OrderComponent', () => {
   let component: OrderComponent;
@@ -11,18 +19,31 @@ describe('OrderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderComponent ]
+      declarations: [ OrderComponent ],
+       providers: [
+        { provide: ActivatedRoute, useClass: FakeActivatedRoute },
+      ],
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(inject([ActivatedRoute], (actRoute: ActivatedRoute) => {
     fixture = TestBed.createComponent(OrderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the thank you message and order reference key', () => {
+
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('#orderReferenceKey').hasAttribute('hidden')).toEqual(false);
+  });
+  
 });
